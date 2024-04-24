68fef755cc9343ec8eb0bc44e4c61f47182977bd
EvKaisin <kaisineugeni@yandex.ru>
2024-04-24 22:02:33 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
 import './app.css'
 
 function App() {
-  const obj = {
-    name :'Alex',
-    nickName : 'Chupik',
-    age: 24,
-    city: 'Kirov'
-  }
-  return (
-    <div className='parent'>
-      {
-        obj.age
-      }
-    </div>
+  const [value, setValue] = useState(Math.floor(Math.random() * 100))
+  const [data, setData] = useState([])
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    fetch('http://universities.hipolabs.com/search?country=United+States')
+      .then((res) => res.json())
+      .then((data) => setData(data.slice(0, 10)))
 
+  })
+  return (
+    <>
+      <div className="center">
+        <input type="number" value={value} />
+        <button onClick={() => { setValue(value * 2) }}>Multi</button>
+        <button onClick={() => { setValue(value / 2) }}>Divide</button>
+        <button onClick={() => { setValue(Math.floor(Math.random() * 100)) }}>UPD</button>
+      </div>
+      <div>
+        <ol>
+          <button onClick={() => { setCount(count + 1) }}>Click</button>
+          {data.map((el, i) => {
+            if (i <= count) {
+              return <div key={i} className='universe'>{i + 1} {el.name}{'   '}{el.web_pages[0]}</div>
+            }
+          })}
+        </ol>
+      </div>
+    </>
   );
 }
 
